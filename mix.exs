f802583771d9417861d67ea88252873ac421de93
derailed <fernand.galiana@gmail.com>
2017-10-26 22:26:51 -0600

    docs update

--- mix.exs
+++ mix.exs
@@ -4,9 +4,9 @@ defmodule ExRay.Mixfile do
   def project do
     [
       app:               :ex_ray,
-      version:           "0.1.0",
+      version:           "0.1.1",
       description:       description(),
-      source_url:        "https://github.om/derailed/aspex",
+      source_url:        "https://github.om/derailed/ex_ray",
       package:           package(),
       docs:              docs(),
       elixir:            "~> 1.5",
