d50c8c7286fee67d3c7a94d89335ddd72e500654
Vladimir Potapev <vladimir.potapev@gmail.com>
2018-11-01 13:38:13 +0700

    Added MVP to pass request_id through functions as an object linked to some pid

--- lib/store.ex
+++ lib/store.ex
@@ -6,7 +6,9 @@ defmodule ExRay.Store do
   scope.
   """
 
-  @table_name :tracers
+  @table_name :ex_ray_tracers_table
+  @request_id_to_pids_table_name :ex_ray_request_id_to_pids_table
+  @pid_to_request_id_table_name :ex_ray_pid_to_request_id_table
 
   require Logger
 
@@ -25,6 +27,24 @@ defmodule ExRay.Store do
         write_concurrency: true
       ]
     )
+    :ets.new(@request_id_to_pids_table_name,
+      [
+        :set,
+        :named_table,
+        :public,
+        read_concurrency:  true,
+        write_concurrency: true
+      ]
+    )
+    :ets.new(@pid_to_request_id_table_name,
+      [
+        :set,
+        :named_table,
+        :public,
+        read_concurrency:  true,
+        write_concurrency: true
+      ]
+    )
   end
 
   @doc """
@@ -32,7 +52,7 @@ defmodule ExRay.Store do
   """
   @spec push(String.t, any) :: any
   def push(key, val) when is_binary(key) do
-   vals = get(key)
+    vals = get(key)
 
     if length(vals) > 0 do
       :ets.insert(@table_name, {key, [val] ++ vals})
@@ -76,5 +96,64 @@ defmodule ExRay.Store do
      []       -> nil
      [h | _t] -> h
     end
+  end
+
+
+  @doc """
+  Links a request_id to corresponding PID, and otherwise. The key must be unique.
+  """
+  @spec link_request_id_and_pid(String.t, pid()) :: :ok
+  def link_request_id_and_pid(request_id, pid) when is_binary(request_id) and is_pid(pid) do
+    pids =
+      case :ets.lookup(@request_id_to_pids_table_name, request_id) do
+        [] -> []
+        [{_request_id, pids}] -> [pid | pids] |> Enum.dedup
+      end
+    :ets.insert(@request_id_to_pids_table_name, {request_id, pids})
+    :ets.insert(@pid_to_request_id_table_name, {pid, request_id})
+    :ok
+  end
+
+  @doc """
+  Returns a pid by linked request id.
+  """
+  @spec get_pids(String.t) :: {:ok, list(pid())} | {:error, :not_found}
+  def get_pids(request_id) when is_binary(request_id) do
+    case :ets.lookup(@request_id_to_pids_table_name, request_id) do
+      [] -> {:error, :not_found}
+      [{_request_id, pids}] -> {:ok, pids}
+    end
+  end
+
+  @doc """
+  Returns a request id by linked pid.
+  """
+  @spec get_request_id(pid()) :: {:ok, String.t} | {:error, :not_found}
+  def get_request_id(pid) when is_pid(pid) do
+    case :ets.lookup(@pid_to_request_id_table_name, pid) do
+      [] -> {:error, :not_found}
+      [{_pid, request_id}] -> {:ok, request_id}
+    end
+  end
+
+  @doc """
+  Remove all records where the request id is a key
+  """
+  @spec remove_request_id(String.t) :: :ok
+  def remove_request_id(request_id) when is_binary(request_id) do
+    case :ets.lookup(@request_id_to_pids_table_name, request_id) do
+      [] -> :ok
+      [{request_id, pids}] ->
+        :ets.delete(@request_id_to_pids_table_name, request_id)
+        Enum.each(pids, &(:ets.delete(@pid_to_request_id_table_name, &1)))
+    end
+  end
+
+  @doc """
+  Remove all records where the pid is a key
+  """
+  @spec remove_pid(pid()) :: :ok
+  def remove_pid(pid) when is_pid(pid) do
+    :ets.delete(@pid_to_request_id_table_name, pid)
   end
 end
